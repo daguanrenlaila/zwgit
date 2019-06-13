55443e53837e7984c3baa26032d1dcbd0257c37e
Allen <1126244470@qq.com>
2019-06-13 09:50:45 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank">
-          核心文档 Dev master to test
+          核心文档 Dev master to test Allen
         </a>
       </li>
       <li>
