e38023eea5ccb13e51babbe49f9d2541730a8e77
Allen <1126244470@qq.com>
2019-06-13 09:53:56 +0800

    master change

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
     <ul>
       <li>
         <a href="https://vuejs.org" target="_blank">
-          核心文档 Dev master to test Allen change to master
+          核心文档
         </a>
       </li>
       <li>
