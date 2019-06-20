19f741190e4927edb6f08ee1ac5a594ace60cb36
Allen <1126244470@qq.com>
2019-06-20 10:12:14 +0800

    once

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,12 +6,6 @@
       <li>
         <a href="https://vuejs.org" target="_blank">
           核心文档
-          新增代码测试一下
-          这是第二次修改
-          这是第三次修改
-          this is master
-          this is addfile
-          我还是旧代码
         </a>
       </li>
       <li>
